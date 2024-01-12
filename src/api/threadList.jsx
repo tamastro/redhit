0bdc719a63ad51343cc0aea39b8673c7cef44538
Rizki Pratama <rpratama45@gmail.com>
2024-01-13 01:22:01 +0700

    integrated graphqlDB

---
+++ src/api/threadList.jsx
@@ -0,0 +1,15 @@
+export const getThread = async () => {
+	const res = await fetch('http://localhost:3000/', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			query: 'query getAllThread {allThreads {id title}}',
+		}),
+	});
+
+	if (!res.ok) {
+		throw new Error('Network response was not ok');
+	}
+
+	return res.json();
+};
